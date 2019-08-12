25e1e23ac6732d5b0954d9d775f9a4d981a230c2
itasouza <itasouza@yahoo.com.br>
2019-08-11 23:52:24 -0300

    Criação da primeira migracao

---
+++ SysApiModelo/src/Api.Data/Migrations/20190812025146_primeira_migracao.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Data.Migrations
+{
+    public partial class primeira_migracao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TB_USUARIO",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Status = table.Column<string>(nullable: true),
+                    Nome = table.Column<string>(maxLength: 120, nullable: false),
+                    Email = table.Column<string>(maxLength: 100, nullable: true),
+                    Login = table.Column<string>(maxLength: 100, nullable: false),
+                    Senha = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_USUARIO", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TB_USUARIO_Email",
+                table: "TB_USUARIO",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TB_USUARIO_Login",
+                table: "TB_USUARIO",
+                column: "Login",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TB_USUARIO");
+        }
+    }
+}
