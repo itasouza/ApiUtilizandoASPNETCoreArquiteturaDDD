691601ed9db6ea70ac135fa9ecb2a0a232873781
itasouza <itasouza@yahoo.com.br>
2019-08-12 23:12:03 -0300

    Conitinuação do projeto

--- SysApiModelo/src/Api.Data/Repository/BaseRepository.cs
+++ SysApiModelo/src/Api.Data/Repository/BaseRepository.cs
@@ -21,9 +21,23 @@ namespace Api.Data.Repository
             _dataset = _context.Set<T>();
         }
 
-        public Task<bool> DeleteAsync(Guid id)
+        public async Task<bool> DeleteAsync(Guid id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var result = await _dataset.SingleOrDefaultAsync(p => p.Id.Equals(id));
+                if (result == null)
+                    return false;
+
+                _dataset.Remove(result);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
         }
 
         public async Task<T> InsertAsync(T item)
@@ -42,21 +56,42 @@ namespace Api.Data.Repository
             }
             catch (Exception ex)
             {
-
                 throw ex;
             }
 
             return item;
         }
 
-        public Task<T> SelectAsync(Guid id)
+
+        public async Task<bool> ExistAsync(Guid id)
         {
-            throw new NotImplementedException();
+            return await _dataset.AnyAsync(p => p.Id.Equals(id));
         }
 
-        public Task<IEnumerable<T>> SelectAsync()
+        public async Task<T> SelectAsync(Guid id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await _dataset.SingleOrDefaultAsync(p => p.Id.Equals(id));
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<IEnumerable<T>> SelectAsync()
+        {
+            try
+            {
+                return await _dataset.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
 
         public async Task<T> UpdateAsync(T item)
@@ -74,10 +109,8 @@ namespace Api.Data.Repository
             }
             catch (Exception ex)
             {
-
                 throw ex;
             }
-
             return item;
         }
     }
