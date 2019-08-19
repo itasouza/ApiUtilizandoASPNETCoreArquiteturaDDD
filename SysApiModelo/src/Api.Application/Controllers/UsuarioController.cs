dd51bbda63c3662291c6aa1cc93ae7b657959769
itasouza <itasouza@yahoo.com.br>
2019-08-19 01:27:59 -0300

    Continuação da aplicação

--- SysApiModelo/src/Api.Application/Controllers/UsuarioController.cs
+++ SysApiModelo/src/Api.Application/Controllers/UsuarioController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Api.Domain.Entities;
 using Api.Domain.Interfaces.Services.Usuario;
 using Microsoft.AspNetCore.Mvc;
 
@@ -38,6 +39,53 @@ namespace Api.Application.Controllers
             catch (ArgumentException e)      
             {
                 return StatusCode((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        //localhost:5000/api/usuario/46464646464
+        [HttpGet]
+        [Route("{id}", Name ="GetWithId")]
+        public async Task<ActionResult> Get(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);//400 bad request - solicitação inválida
+            }
+
+            try
+            {
+                return Ok(await _service.Get(id));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] UsuarioEntity user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);//400 bad request - solicitação inválida
+            }
+
+            try
+            {
+                var result = await _service.Post(user);
+                if(result != null)
+                {
+                    return Created(new Uri (Url.Link("GetWithId", new { id = result.Id })),result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
             }
         }
 
